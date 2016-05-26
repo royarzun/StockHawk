3d2433a31af7c250cdd7b007895d85dacfd5af27
Ricardo Oyarzun <royarzun@gmail.com>
2016-05-25 22:29:56 -0400

    Remove repeated symbol string Constant @ StockIntentService

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -10,7 +10,6 @@ import com.google.android.gms.gcm.TaskParams;
  * Created by sam_chordas on 10/1/15.
  */
 public class StockIntentService extends IntentService {
-  private static final String ARG_SYMBOL = "symbol";
 
   public static final String EXTRA_TAG = "tag";
   public static final String EXTRA_SYMBOL = "symbol";
@@ -31,7 +30,7 @@ public class StockIntentService extends IntentService {
     StockTaskService stockTaskService = new StockTaskService(this);
     Bundle args = new Bundle();
     if (intent.getStringExtra(EXTRA_TAG).equals(ACTION_ADD)){
-      args.putString(ARG_SYMBOL, intent.getStringExtra(EXTRA_SYMBOL));
+      args.putString(EXTRA_SYMBOL, intent.getStringExtra(EXTRA_SYMBOL));
     }
     // We can call OnRunTask from the intent service to force it to run immediately instead of
     // scheduling a task.
