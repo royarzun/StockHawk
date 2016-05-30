2b4e1e7e5f34f893ce2db39fb5e8e24733681b4f
Ricardo Oyarzun <royarzun@gmail.com>
2016-05-29 23:48:43 -0400

    Rollback stock list to the main activity
    I had moved everything into fragments, going back
    to work with the activity. Adding a scrolling toolbar
    for better looks.

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -1,33 +1,54 @@
 package com.sam_chordas.android.stockhawk.ui;
 
+import android.app.LoaderManager;
 import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
 import butterknife.ButterKnife;
 import butterknife.Bind;
+import butterknife.OnClick;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.melnykov.fab.FloatingActionButton;
 import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
 import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+import com.sam_chordas.android.stockhawk.rest.QuoteCursorAdapter;
+import com.sam_chordas.android.stockhawk.rest.RecyclerViewItemClickListener;
 import com.sam_chordas.android.stockhawk.rest.Utils;
 import com.sam_chordas.android.stockhawk.service.StockIntentService;
-import com.sam_chordas.android.stockhawk.ui.fragments.StocksFragment;
 
 
-public class MyStocksActivity extends AppCompatActivity {
+public class MyStocksActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
 
     /**
     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
     */
+    private static final int CURSOR_LOADER_ID = 0;
 
-    @Bind(R.id.my_stocks_toolbar) Toolbar toolbar;
+    private QuoteCursorAdapter mCursorAdapter;
+
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.fab) FloatingActionButton fab;
+    @Bind(R.id.recycler_view) RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,16 +70,74 @@ public class MyStocksActivity extends AppCompatActivity {
                 networkToast();
             }
         }
-        StocksFragment fragment = StocksFragment.newInstance();
-        getSupportFragmentManager().beginTransaction()
-                                    .replace(R.id.stocks_fragment, fragment)
-                                    .addToBackStack(null)
-                                    .commit();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mCursorAdapter = new QuoteCursorAdapter(this, null);
+
+        recyclerView.addOnItemTouchListener(new RecyclerViewItemClickListener(this,
+                new RecyclerViewItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View v, int position) {
+
+                    }
+                }));
+        recyclerView.setAdapter(mCursorAdapter);
+        fab.attachToRecyclerView(recyclerView);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.fab)
+    public void OnClickingTheFabButton() {
+        if (isNetworkAvailable()){
+            new MaterialDialog.Builder(this).title(R.string.symbol_search)
+                    .content(R.string.content_test)
+                    .inputType(InputType.TYPE_CLASS_TEXT)
+                    .input(R.string.input_hint, R.string.input_prefill,
+                            new MaterialDialog.InputCallback() {
+                                @Override
+                                public void onInput(MaterialDialog dialog, CharSequence input) {
+                                    // On FAB click, receive user input. Make sure the stock doesn't
+                                    // already exist in the DB and proceed accordingly
+                                    addQuoteOrFailGracefully(input.toString());
+                                }
+                            })
+                    .show();
+        } else {
+            networkToast();
+        }
+    }
+
+    private void addQuoteOrFailGracefully(String input){
+        Cursor c = getContentResolver()
+                .query(QuoteProvider.Quotes.CONTENT_URI,
+                        new String[] {
+                                QuoteColumns.SYMBOL
+                        }, QuoteColumns.SYMBOL + "= ?",
+                        new String[] {
+                                input
+                        }, null);
+        if (c != null && c.getCount() != 0) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "This stock is already saved!", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER, Gravity.CENTER, 0);
+            toast.show();
+            c.close();
+            return;
+        } else {
+            // Add the stock to DB
+            Intent mServiceIntent = new Intent(getApplicationContext(),
+                    StockIntentService.class);
+            mServiceIntent.putExtra(StockIntentService.EXTRA_TAG,
+                    StockIntentService.ACTION_ADD);
+            mServiceIntent.putExtra(StockIntentService.EXTRA_SYMBOL, input);
+            startService(mServiceIntent);
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        getLoaderManager().restartLoader(CURSOR_LOADER_ID, null, this);
     }
 
     public void networkToast(){
@@ -95,4 +174,33 @@ public class MyStocksActivity extends AppCompatActivity {
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // This narrows the return to only the stocks that are most current.
+        String[] columnsIds = new String[]{
+                QuoteColumns._ID,
+                QuoteColumns.SYMBOL,
+                QuoteColumns.BIDPRICE,
+                QuoteColumns.PERCENT_CHANGE,
+                QuoteColumns.CHANGE, QuoteColumns.ISUP
+        };
+        return new CursorLoader(this, QuoteProvider.Quotes.CONTENT_URI, columnsIds,
+                QuoteColumns.ISCURRENT + " = ?",
+                new String[]{"1"},
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (mCursorAdapter != null){
+            mCursorAdapter.swapCursor(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
+    }
+
 }
